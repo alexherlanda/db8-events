9cc61f986dc13bbdbe7ea662475461a3f2dd10a1
Alexis Herlanda <alexisherlanda@gmail.com>
2020-06-04 21:39:11 -0500

    Creates model od events data

---
+++ src/models/events/index.js
@@ -0,0 +1,14 @@
+export const events = [
+  {
+    name: 'Copa UNAM',
+    link: 'www.facebook.com',
+    host: 'Debate FESC',
+    reunionSpot: 'Zoom',
+    country: 'México',
+    description: 'B',
+    tags: {
+      attendanceType: 'Virtual',
+      category: 'Torneo',
+    },
+  },
+];
