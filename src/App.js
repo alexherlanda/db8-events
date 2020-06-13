0e82dfb90297b6f8f1d51ad5c7ca6f0495a9dfd5
Alexis Herlanda <alexisherlanda@gmail.com>
2020-06-13 17:09:50 -0500

    Adds Main Bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Layout, List, Card, Row, Col } from 'antd';
-import './App.css';
+import { Layout, List, Card, Row, Col, Typography } from 'antd';
 import './App.less';
 import { realEvents } from './models/events';
 import EventCard from './components/molecular/EventCard';
@@ -14,7 +13,34 @@ function App(props) {
         <Content style={{ margin: 20 }}>
           <Row>
             <Col span={24}>
-              <Card style={{ borderRadius: '12px', marginBottom: '12px' }} />
+              <Card
+                bodyStyle={{ padding: 0 }}
+                style={{ borderRadius: '22px', marginBottom: '12px', padding: 0 }}
+              >
+                <Row
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    minHeight: '50px',
+                    alignItems: 'flex-end',
+                  }}
+                >
+                  <Col
+                    style={{
+                      alignItems: 'flex-end',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      fontSize: '15px',
+                      fontWeight: 'bold',
+                      borderBottom: '2px solid #1890ff',
+                      height: '100%',
+                      padding: '10px',
+                    }}
+                  >
+                    Explorar
+                  </Col>
+                </Row>
+              </Card>
             </Col>
           </Row>
           <List
@@ -22,7 +48,7 @@ function App(props) {
               gutter: 8,
               xs: 1,
               sm: 2,
-              md: 3,
+              md: 2,
               lg: 3,
               xl: 3,
               xxl: 3,
