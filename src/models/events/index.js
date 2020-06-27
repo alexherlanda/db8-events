d6b5de41ac2e69c85206ebc680ae03072478b86b
Alexis Herlanda <alexisherlanda@gmail.com>
2020-06-26 22:04:58 -0500

    adds more events

--- src/models/events/index.js
+++ src/models/events/index.js
@@ -43,7 +43,49 @@ export const realEvents = [
       { key: 'formatType', text: 'Otro' },
     ],
   },
-
+  {
+    convenorsShortName: 'Sociedad de Debate UCAB',
+    convenorsCompleteName: 'Sociedad de Debate UCAB',
+    country: 'VE',
+    countryCode: 'VE',
+    coverUrl: 'https://live.staticflickr.com/65535/50048891066_b5d6fbbbcb_b.jpg',
+    description:
+      'Torneo BP. Incluye categoría novatos. Premios de diplomados al mejor juez y al equipo campeón',
+    startDate: '2020-07-10',
+    endDate: '2020-07-12',
+    infoLink: 'https://instagram.com/ucab_debate?igshid=v1m1bi140xkq',
+    name: 'Copa Andrés Bello',
+    shortName: 'Copa Andrés Bello',
+    registerLink:
+      'https://docs.google.com/forms/d/1PW8m7B4i7HoDeMM0ksT-8fu7cDF_zJ9t4AC2i2UPRiE/edit?chromeless=1',
+    reunionSpot: 'Discord',
+    tags: [
+      { key: 'type', text: 'Torneo' },
+      { key: 'attendanceType', text: 'Virtual' },
+      { key: 'formatType', text: 'BP' },
+    ],
+  },
+  {
+    convenorsShortName: 'Círculo de debate San Marcos',
+    convenorsCompleteName: 'Círculo de debate San Marcos y OdSM',
+    country: 'PE',
+    countryCode: 'PE',
+    coverUrl: 'https://live.staticflickr.com/65535/50045728437_72b4a02ec4_b.jpg',
+    description:
+      'Torneo OPEN de antesala al Torneo de Novatos (ToNo). S/.5 o $2 por oradorx. Jueces no pagan. Premios en dinero para top speaker, top juezx y equipo ganador. Parte del dinero recaudado es para los premios del ToNo, que será gratuito',
+    startDate: '2020-07-11',
+    endDate: '2020-07-12',
+    infoLink: 'https://web.facebook.com/events/559516174713615/',
+    name: 'Las Previas del ToNo 2da Edición',
+    shortName: 'Pre ToNo 2',
+    registerLink: 'https://forms.gle/9oEtBihwmqwNY7uh8 ',
+    reunionSpot: 'Discord',
+    tags: [
+      { key: 'type', text: 'Torneo' },
+      { key: 'attendanceType', text: 'Virtual' },
+      { key: 'formatType', text: 'BP' },
+    ],
+  },
   {
     convenorsShortName: 'USFQ',
     convenorsCompleteName: 'USFQ y CMUDE 2020',
@@ -64,7 +106,28 @@ export const realEvents = [
       { key: 'formatType', text: 'BP' },
     ],
   },
-
+  {
+    convenorsShortName: 'Círculo de debate San Marcos',
+    convenorsCompleteName:
+      'Círculo de debate San Marcos y Universidad Nacional Mayor de San Marcos',
+    country: 'PE',
+    countryCode: 'PE',
+    coverUrl: 'https://live.staticflickr.com/65535/50045728437_72b4a02ec4_b.jpg',
+    description:
+      'El ToNo nace para brindar espacios a novatxs de aprendizaje y comodidad, pero no solo eso, traemos también: 9 rondas novatos, 7 rondas máster (jueces), 200 dólares en premios, inscripción gratuita, competencia de discursos virtual y novedosa, talleres, breafing de mociones',
+    startDate: '2020-07-17',
+    endDate: '2020-07-26',
+    infoLink: 'https://www.facebook.com/ToNo.OdSM/',
+    name: 'Torneo de Novatxs Intersociedades de Debate',
+    shortName: 'ToNo',
+    registerLink: 'https://www.facebook.com/ToNo.OdSM/',
+    reunionSpot: 'Discord',
+    tags: [
+      { key: 'type', text: 'Torneo' },
+      { key: 'attendanceType', text: 'Virtual' },
+      { key: 'formatType', text: 'BP' },
+    ],
+  },
   {
     convenorsShortName: 'Argumentum ESPOL',
     convenorsCompleteName: 'Argumentum ESPOL',
