d2aece2e8c27665fc7ac490af61f9fef1bba3248
adrianoduque97 <adrian.duque@capmation.com>
2020-07-06 19:08:10 -0500

    Undo Changes on the routes

--- server/src/controllers/events.js
+++ server/src/controllers/events.js
@@ -3,16 +3,3 @@ import Event from '../models/Event'
 export const getEvents = async (req, res) => {
   res.json({ message: 'Events' })
 }
-
-export const getEvent =  async(req, res) =>{
-  const id = req.params.id
-  res.json({message:"Event eith id"})
-}
-
-
-export const newEvent = async(req,res)=>{
-  const model = req.body.event
-
-  //TODO add to the mongo db
-  res.json({message: `Added ${model}`})
-}
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,22 +1,10 @@
 import express from 'express'
 
-import { getEvents, getEvent } from '../controllers/events'
 
 var router = express.Router()
 
 /* GET all events */
 router.get('/events', getEvents)
 
-/* GET one event by iq */
-router.get('/events/:id', getEvent)
-
-/* POST create new event*/
-//TODO on the body form, receive it as as array of element[]
-router.post('/events')
-
-/*PUT update an event*/
-router.put('/events/:id')
-/*DELETE delete an event*/
-router.delete('/events')
 
 export default router
