5a9599b8a618825322cf6bd4c03778cab57470c6
adrianoduque97 <adrian.duque@capmation.com>
2020-07-06 19:01:50 -0500

    Passport SetUo and User Schema

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,10 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import apiRouter from './routes/api'
 import { connectDB } from './database'
+import passport from 'passport'
+import LocalStrategy from 'passport-local'
+import User from "./models/User"
+
 
 const app = express()
 
@@ -15,6 +19,19 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+//Passport Setup
+app.use(require("express-session")({
+  secret: "db8community",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 // Static files
 if (process.env.NODE_ENV === 'production') {
