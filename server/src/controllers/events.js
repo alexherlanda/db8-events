d696f7192544fcc14250d565e6de2461f1384f46
adrianoduque97 <adrian.duque@capmation.com>
2020-07-06 19:32:16 -0500

    Fix over routes

--- server/src/controllers/events.js
+++ server/src/controllers/events.js
@@ -1,5 +1,4 @@
 import Event from '../models/Event'
-import { getEvents } from '../controllers/events'
 
 export const getEvents = async (req, res) => {
   res.json({ message: 'Events' })
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import { getEvents } from '../controllers/events'
 
 var router = express.Router()
 
