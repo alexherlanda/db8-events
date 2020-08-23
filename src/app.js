65ace1db171fc97799afc7f8e50bb695c4712a8d
Alex Herlanda <alexisherlanda@gmail.com>
2020-08-23 15:24:43 -0500

    hot fix status 413 🐛

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ connectDB()
 
 // Middlewares
 app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({limit: '50mb', extended: true}))
+app.use(express.urlencoded({limit: '50mb', extended: false, parameterLimit: 50000 }))
 app.use(cookieParser())
 
 // Passport Setup
